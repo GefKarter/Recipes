864666acdedcb44607f4227b926037d85971957b
GefKarter <gefkarter@gmail.com>
2023-10-31 07:42:21 +1100

    F 4.6
    Recipebook

--- dish_recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from .api import *
-
-
-router = routers.DefaultRouter()
-router.register('api/recipe', RecipeViewset, 'dish_recipes')
-
-
-urlpatterns = router.urls
--- dish_recipes/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from .models import *
-from .serializers import *
-from rest_framework.response import Response
